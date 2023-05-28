b640af732afe069f877ceea7b3c7dbd987f6b099
George Daskalakis <george.dask97@gmail.com>
2023-05-28 20:27:40 +0300

    chore: fix typo

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 
   kit: {
     paths: {
-      assets: 'https://geodask-ics.netlify.com'
+      assets: 'https://geodask-ics.netlify.app'
     },
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
